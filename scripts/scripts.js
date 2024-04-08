5537058e185973354ffdcb882e09ca294d8ca21e
Zach Rundle <157517004+zrundle@users.noreply.github.com>
2024-04-08 01:47:28 -0500

    COMMIT

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,12 +6,6 @@ $("#userName").keyup(function (e) {
 
 });
 
-$("#passWord").keyup(function (e) {
-
-    e.preventDefault();
-    this.value = this.value.replace(/[^a-z]/g, "");
-    
-});
 
 //Data validation for numbers only
 $("#cardNumber").keyup(function (e) {
@@ -33,4 +27,23 @@ $("#cvvNumber").keyup(function (e) {
     e.preventDefault();
     this.value = this.value.replace(/[^0-9\.]/g, "");
 
+});
+
+
+$(function() {
+    console.log( "ready!" );
+  
+  //click the eye icon w/class reveal 
+  $(".reveal").on('click',function() {
+    let $pwd = $(".pwd");
+    
+    if ($pwd.attr('type') === 'password') {
+        $pwd.attr('type', 'text');
+       $(this).children('i').addClass('bi-eye-slash-fill').removeClass('bi-eye-fill');
+    } else {
+        $pwd.attr('type', 'password');
+     $(this).children('i').removeClass('bi-eye-slash-fill').addClass('bi-eye-fill');
+    }
+});
+  
 });
